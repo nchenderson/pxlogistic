4201c19eefac7de6550c62434008bf12adb66a78
Nicholas Henderson <nchender@Nicholass-iMac.attlocal.net>
2020-08-01 00:39:46 -0400

    changed format for control parameters

--- R/pxlogistic.R
+++ R/pxlogistic.R
@@ -1,26 +1,41 @@
-pxlogistic <- function(beta.init, X, y, n, omega=0, method="Newton", tol=1e-4, max.iter=500){
-  beta.old <- theta <- beta.init
-  kap <- y - n/2
+pxlogistic <- function(beta.init, X, y, n.trials=rep(1, length(y)), control=list()) {
+  
+  control.default <- list(maxiter=2000, tol=1e-7, method="Newton")
+  namc <- names(control)
+  if (!all(namc %in% names(control.default))) {
+    stop("unknown names in control: ", namc[!(namc %in% names(control.default))])
+  }
+  control <- modifyList(control.default, control)
+  
+  maxiter <- control$maxiter
+  tol <- control$tol
+  method <- control$method 
+  
+  beta.old <- beta.init
+  kap <- y - n.trials/2
   iter <- 0
   alpha.old <- 1
-  obj <- function(alp) -sum(alp*y*x.theta-log1p(exp(alp*x.theta)))
-  while(TRUE){
-    phi <- as.numeric(X%*%beta.old)
-    ww <- as.numeric((n/(2*phi))*tanh(phi/2))
-    theta <- (1+omega)*solve(crossprod(X, X*ww), crossprod(X, kap))/alpha.old - omega*theta
-    x.theta <- as.numeric(X%*%theta)
-    if(method=="Newton") {
-      alpha.new <- UpdateAlphaNewton(alpha.old, x.theta, y, init=!iter)
-    } else if(method=="BFGS") {
-      it <- ifelse(iter==0, 1e4, 1)
-      alpha.new <- optim(alpha.old, obj, method="BFGS", control=list(maxit=it))$par
-    }
-    beta.new <- alpha.new*theta
-    
-    iter <- iter + 1
-    if(norm(beta.new-beta.old, "2") < tol | iter > max.iter) break
-    beta.old <- beta.new
-    alpha.old <- alpha.new
+  obj <- function(alp) {
+      -sum(alp*y*x.theta - n.trials*log1p(exp(alp*x.theta)) )
   }
-  return(list(beta=beta.new, iter=iter))
+  while(TRUE) {
+      phi <- as.numeric(X%*%beta.old)
+      ww <- as.numeric((n.trials/(2*phi))*tanh(phi/2))
+      theta <- solve(crossprod(X, X*ww), crossprod(X, kap))/alpha.old
+      ## maybe add the MO in another function
+      x.theta <- as.numeric(X%*%theta)
+      if(method=="Newton") {
+          alpha.new <- UpdateAlphaNewton(alpha.old, x.theta, y, init=!iter)
+      } else if(method=="BFGS") {
+         it <- ifelse(iter==0, 1e4, 1)
+         alpha.new <- optim(alpha.old, obj, method="BFGS", control=list(maxit=it))$par
+      }
+      beta.new <- alpha.new*theta
+    
+      iter <- iter + 1
+      if(norm(beta.new-beta.old, "2") < tol | iter > max.iter) break
+      beta.old <- beta.new
+      alpha.old <- alpha.new
+  }
+  return(list(coef=beta.new, iter=iter))
 }
