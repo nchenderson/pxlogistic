f0150905fe24efd1879d5bb1b466d0aa44c56ab6
Nicholas Henderson <nchender@NicholassiMac2.attlocal.net>
2020-12-16 16:00:02 -0500

    change to loop stopping condition

--- R/UpdateAlphaNewton.R
+++ R/UpdateAlphaNewton.R
@@ -8,10 +8,12 @@ UpdateAlphaNewton <- function(alpha.old, x.theta, y, n.trials, init=FALSE) {
   while(TRUE){
      ept <- plogis(alpha.old*x.theta)
      dept <- ept*(1-ept)
+     
      f.st <- sum(x.theta*(y - n.trials*ept))
+     if(loop==FALSE | abs(f.st) < 1e-7) break
+     
      f.nd <- sum(n.trials*x.theta*x.theta*dept)
      alpha.new <- as.numeric(alpha.old + f.st/f.nd)
-     if(loop==FALSE | abs(f.st) < 1e-7) break
      alpha.old <- alpha.new
   }
   ans <- alpha.new
