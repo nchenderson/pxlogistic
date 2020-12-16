9e72e184b0fe4a4eea19ec255df6a9847cca3785
Nicholas Henderson <nchender@NicholassiMac2.attlocal.net>
2020-12-16 15:59:31 -0500

    added objective function tracking option

--- R/pxlogistic.R
+++ R/pxlogistic.R
@@ -1,4 +1,5 @@
-pxlogistic <- function(par, X, y, n.trials=rep(1, length(y)), lambda=NULL, control=list()) {
+pxlogistic <- function(par, X, y, n.trials=rep(1, length(y)), lambda=NULL, track.objective=FALSE,
+                       control=list()) {
   
   control.default <- list(maxiter=2000, tol=1e-7, method="Newton")
   namc <- names(control)
@@ -10,6 +11,7 @@ pxlogistic <- function(par, X, y, n.trials=rep(1, length(y)), lambda=NULL, contr
   maxiter <- control$maxiter
   tol <- control$tol
   method <- control$method 
+  objfn.track <- NULL
   
   beta.init <- par
   beta.old <- beta.init
@@ -23,17 +25,22 @@ pxlogistic <- function(par, X, y, n.trials=rep(1, length(y)), lambda=NULL, contr
       -sum(alp*y*x.theta - n.trials*log1p(exp(alp*x.theta)) - lam*alp*alp*theta*theta/2)
   }
   ww <- rep(0, length(y))
+  phi <- as.numeric(X%*%beta.old)
+  if(track.objective) {
+      objfn.track = rep(NA, maxiter + 1)
+      objfn.track[1] <- LogLikObserved(1, phi)
+  }
+   
+  
   while(TRUE) {
       ## Compute weight vector
-      phi <- as.numeric(X%*%beta.old)
+      phat <- plogis(phi)
       small.phi <- abs(phi) < 1e-4
       ww[small.phi] <- n.trials[small.phi]*(1/4 - (phi[small.phi]^2)/48 + (phi[small.phi]^4)/480)
-      ww[!small.phi] <- as.numeric((n.trials[!small.phi]*tanh(phi[!small.phi]/2))/(2*phi[!small.phi])) 
+      ww[!small.phi] <- as.numeric((n.trials[!small.phi]*(phat[!small.phi] - 1/2))/phi[!small.phi]) 
       
-      #XtWX <- crossprod(X, X*ww)
       theta <- solve(crossprod(X, X*ww), crossprod(X, uvec))/alpha.old
       x.theta <- as.numeric(X%*%theta)
-      #print('hello2')
       if(method=="Newton") {
           alpha.new <- UpdateAlphaNewton(alpha.old, x.theta, y, n.trials, init=TRUE)
       } else if(method=="BFGS") {
@@ -48,6 +55,32 @@ pxlogistic <- function(par, X, y, n.trials=rep(1, length(y)), lambda=NULL, contr
       if(norm(beta.new-beta.old, "2") < tol | iter >= maxiter) break
       beta.old <- beta.new
       alpha.old <- alpha.new
+      
+      phi <- as.numeric(X%*%beta.old)
+      if(track.objective) {
+          objfn.track[iter+1] <- LogLikObserved(1, phi)
+      }
   }
-  return(list(coef=beta.new, iter=iter))
+  if(track.objective) {
+      objfn.track <- objfn.track[!is.na(objfn.track)]
+  }
+  return(list(coef=beta.new, iter=iter, objfn.track=objfn.track))
 }
+
+
+#UpdateAlphaNewton <- function(alpha.old, x.theta, y, n, init=FALSE){
+#  if(init) alpha.old <- 0
+  
+#  alpha.new <- alpha.old
+#  while(TRUE){
+#    ept <- expit(alpha.old*x.theta)
+#    dept <- ept*(1-ept)
+#    f.st <- sum(x.theta*(y - n*ept))
+#    if(abs(f.st) < 1e-8) break
+#    f.nd <- sum(x.theta^2*n*dept)
+#    alpha.new <- as.numeric(alpha.old + f.st/f.nd)
+#    alpha.old <- alpha.new
+#  }
+#  ans <- alpha.new
+#  return(ans)
+#}
