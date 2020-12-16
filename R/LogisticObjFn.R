cb99402087f29a2a0bf4d38a766484f27c315034
Nicholas Henderson <nchender@NicholassiMac2.attlocal.net>
2020-12-16 15:58:03 -0500

    Changed objective function so it can handle differing number of trials

--- R/LogisticObjFn.R
+++ R/LogisticObjFn.R
@@ -1,7 +1,8 @@
-LogisticObjFn <- function(par, X, y) {
+LogisticObjFn <- function(par, X, y, n.trials=rep(1, length(y))) {
   
   ## Use Log-Sum-Exp trick for more stable computation of the log-likelihood
   x.beta <- as.numeric(X%*%par)
-  ans <- sum(y*x.beta) - sum(log1p(exp(x.beta[x.beta < 0]))) - sum(x.beta[x.beta >= 0]) - sum(log1p(exp(-x.beta[x.beta >= 0])))
+  lt.zero <- x.beta < 0
+  ans <- sum(y*x.beta) - sum(n.trials[lt.zero]*log1p(exp(x.beta[lt.zero]))) - sum(n.trials[!lt.zero]*x.beta[!lt.zero]) - sum(n.trials[!lt.zero]*log1p(exp(-x.beta[!lt.zero])))
   return(ans)
 }
